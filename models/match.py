f2935fa294620a7cfd47adbaa64cb1e61f27becd
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-03-21 15:51:09 +0100

    feat(math model): add method to return scores

--- models/match.py
+++ models/match.py
@@ -1,11 +1,11 @@
 from models.player import Player
-import datetime
+
 
 class Match:
     def __init__(self, player1: Player, player2: Player, score_player1: float = 0, score_player2: float = 0):
         self.player1 = player1
         self.player2 = player2
-        self.score_player1 = score_player1,
+        self.score_player1 = score_player1
         self.score_player2 = score_player2
 
     def player1_wins(self):
@@ -19,3 +19,6 @@ class Match:
     def drown(self):
         self.score_player1 += 0.5
         self.score_player2 += 0.5
+
+    def get_scores(self):
+        return self.score_player1, self.score_player2
