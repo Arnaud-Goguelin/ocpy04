93e49e62acd68b3dbf7f9e0f6a57762eb2ecc3f2
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-05-01 23:45:20 +0200

    feat(contsants et main controller): reorganize generic message in an enum and use some messages in main controller + call tournamÃent controller in main controller

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,11 +1,12 @@
 # need to import others controllers with an absolut path to avoid the circular import issue.
 from typing import TYPE_CHECKING
 
-from controllers.player.player_controller import PlayerController
+from .player.player_controller import PlayerController
+from .tournament.tournament_controller import TournamentController
 
 if TYPE_CHECKING:
     from main import Data
-from utils import EXIT_MESSAGE
+from utils import GenericMessages, CANCELLED_INPUT
 from views import MainMenuView
 
 
@@ -21,9 +22,10 @@ class MainController:
             player_controller = PlayerController(self.data)
             player_controller.handle_player_main_menu()
         elif choice == "2":
-            pass
-        elif choice == "3":
-            print(EXIT_MESSAGE)
+            tournament_controller = TournamentController(self.data)
+            tournament_controller.handle_tournament_main_menu()
+        elif choice == CANCELLED_INPUT:
+            print(GenericMessages.EXIT_MESSAGE.value)
             exit(0)
         else:
             raise ValueError("Invalid option, please choose between 1, 2 or 3.")
--- utils/constant.py
+++ utils/constant.py
@@ -1,22 +1,32 @@
+from enum import Enum
+
 MAX_NUMBER_OF_ROUNDS = 4
-EXIT_MESSAGE = r"""
-        Thanks you and goodbye.
-                ,....,
-              ,::::::<
-             ,::/^\"``.
-            ,::/, `   e`.
-           ,::; |        '.
-           ,::|  \___,-.  c)
-           ;::|     \   '-'
-           ;::|      \
-           ;::|   _.=`\
-           `;:|.=` _.=`\
-             '|_.=`   __\
-             `\_..==`` /
-              .'.___.-'.
-             /          \
-            ('--......--')
-            /'--......--'\
-            `"--......--"
-        Knight by Joan G. Stark
-"""
+CANCELLED_INPUT = "q"
+
+
+class GenericMessages(str, Enum):
+    PLAYER_MENU_RETURN = "Go back to Player menu in ... "
+    TOURNAMENT_MENU_RETURN = "Go back to Tournament menu in ... "
+    MAIN_MENU_RETURN = "Go back to Main menu in ... "
+    PLAYER_LIST = "Player list display in ... "
+    EXIT_MESSAGE = r"""
+            Thanks you and goodbye.
+                    ,....,
+                  ,::::::<
+                 ,::/^\"``.
+                ,::/, `   e`.
+               ,::; |        '.
+               ,::|  \___,-.  c)
+               ;::|     \   '-'
+               ;::|      \
+               ;::|   _.=`\
+               `;:|.=` _.=`\
+                 '|_.=`   __\
+                 `\_..==`` /
+                  .'.___.-'.
+                 /          \
+                ('--......--')
+                /'--......--'\
+                `"--......--"
+            Knight by Joan G. Stark
+    """
