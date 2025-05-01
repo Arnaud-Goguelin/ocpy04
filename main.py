037b3c8a050e28f2f652840f245098eb03a14d5d
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-05-01 23:48:00 +0200

    feat(mpain): create a test tournament in Data and use generic message enum

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from controllers import MainController
-from models.player import Player
-from utils import countdown
+from models import Player, Tournament
+from utils import countdown, GenericMessages
 
 
 class Data:
     def __init__(self):
-        # TODO begin with players instances
         self.players = [
             Player(first_name="John", last_name="Johnson", birthdate="03-03-1973", chess_id="EF23456"),
             Player(first_name="Mary", last_name="Marydotter", birthdate="04-04-1974", chess_id="GH78910"),
             Player(first_name="Alice", last_name="Alicedotter", birthdate="02-02-1972", chess_id="CD67891"),
             Player(first_name="Bob", last_name="Bobson", birthdate="01-01-1970", chess_id="AB12345"),
+        ]
+        self.tournaments = [
+            Tournament(
+                name="Tournament Test",
+                location=("some where..."),
+                description=("...over the seas"),
+                players={
+                    Player(first_name="John", last_name="Johnson", birthdate="03-03-1973", chess_id="EF23456"),
+                    Player(first_name="Mary", last_name="Marydotter", birthdate="04-04-1974", chess_id="GH78910"),
+                    Player(first_name="Alice", last_name="Alicedotter", birthdate="02-02-1972", chess_id="CD67891"),
+                    Player(first_name="Bob", last_name="Bobson", birthdate="01-01-1970", chess_id="AB12345"),
+                },
+            )
         ]
 
     def save(self):
@@ -37,7 +49,7 @@ class Application:
             except Exception as error:
                 print("\nAn error occurred : ")
                 print(error)
-                countdown(menu_name="main")
+                countdown(GenericMessages.MAIN_MENU_RETURN.value)
 
 
 if __name__ == "__main__":
