b866d8adcff8bc91375fea27600800d531dc1487
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-06-06 10:01:39 +0200

    feat(data): create load method for data and call it before app display first menu

--- models/data.py
+++ models/data.py
@@ -10,24 +10,8 @@ from utils import DataFilesNames
 
 class Data:
     def __init__(self):
-        self.players = [
-            Player(first_name="John", last_name="Johnson", birthdate="03-03-1973", chess_id="EF23456"),
-            Player(first_name="Mary", last_name="Marydotter", birthdate="04-04-1974", chess_id="GH78910"),
-            Player(first_name="Alice", last_name="Alicedotter", birthdate="02-02-1972", chess_id="CD67891"),
-            Player(first_name="Bob", last_name="Bobson", birthdate="01-01-1970", chess_id="AB12345"),
-            Player(first_name="Jane", last_name="Janedotter", birthdate="05-05-1975", chess_id="AB12345"),
-            Player(first_name="Jill", last_name="Jilldotter", birthdate="06-06-1976", chess_id="MN34567"),
-            Player(first_name="Peter", last_name="Peterson", birthdate="07-07-1977", chess_id="IJ34567"),
-            Player(first_name="Sarah", last_name="Sarahdotter", birthdate="08-08-1978", chess_id="KL89012"),
-        ]
-        self.tournaments = [
-            Tournament(
-                name="Tournament Test",
-                location=("some where..."),
-                description=("...over the seas"),
-                players={player for player in self.players},
-            )
-        ]
+        self.players = []
+        self.tournaments = []
         self.data_folder = "data"
 
     # TODO be sure data folder exist on remote repository and be sure file are created if they do not exist
@@ -57,12 +41,31 @@ class Data:
         except (json.JSONDecodeError, TypeError) as error:
             raise Exception(f"Error saving in {file_name.value}: {error}")
 
-    def load(self):
-        pass
-        # TODO from a JSON file complete attributes of Data class
-        # To call when app open
-        # depuis le ficheir json, créer une isntance de chaque modèle pour chaque dict dans le fichier
+    def load(self) -> None:
 
+        self.validate_directory()
+
+        file_mappings = {
+            DataFilesNames.PLAYERS_FILE: Player,
+            DataFilesNames.TOURNAMENTS_FILE: Tournament,
+        }
+
+        for file_name, model in file_mappings.items():
+            try:
+                file_path = os.path.join(self.data_folder, file_name.value)
+
+                if os.path.exists(file_path):
+                    with open(file_path, "r", encoding="utf-8") as file:
+                        data = json.load(file)
+                        instances = [model.from_dict(item_dict) for item_dict in data]
+                        setattr(self, f"{model.__name__.lower()}s", instances)
+
+            except (json.JSONDecodeError, TypeError) as error:
+                raise Exception(f"Error loading in {file_name.value}: {error}")
+
+        return None
+
+    # TODO: delete once dev is finished
     def erase(self) -> None:
         for file_name in DataFilesNames:
             try:
