1255e4f829f64a82e2a083e6bae511310169555a
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-06-15 18:26:12 +0200

    fix(tournament controller and list): fix value error handling in tournmanet list display

--- views/tournament/tournament_list.py
+++ views/tournament/tournament_list.py
@@ -5,7 +5,7 @@ from colorama import Fore
 from utils import (
     CANCELLED_INPUT,
     print_title,
-)
+    )
 
 if TYPE_CHECKING:
     from models.tournament import Tournament
@@ -26,7 +26,6 @@ class TournamentListView:
     def handle_tournaments_list(cls, tournaments: list["Tournament"]):
         print_title("\u265b Tournaments List \u2655 :")
 
-        # TODO: raise value error stop app, handle it in controller
         if not tournaments:
             raise ValueError("No tournaments to display.")
 
